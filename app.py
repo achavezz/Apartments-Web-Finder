f09c3c2e9541269e42d4b60a910a2512e1824640
Alex Chavez <achavez@NDF9YXVPDX.local>
2024-02-21 07:52:24 -0600

    Update

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://www.apartments.com/")
 
-location = input("Press Enter to close the browser: ")
+location = input("Enter location: ")
 
 element_quickSearch = driver.find_element(By.ID, "quickSearchLookup")
 element_quickSearch.clear()
 element_quickSearch.send_keys(location)
 element_quickSearch.send_keys(Keys.RETURN)
 
+input("Waiting...")
 driver.close()
