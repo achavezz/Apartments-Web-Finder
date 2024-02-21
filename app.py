5da9851a287fca144831f4a26510a90c828cd015
Alex Chavez <achavez@NDF9YXVPDX.local>
2024-02-21 08:46:23 -0600

    Update files

--- app.py
+++ app.py
@@ -2,15 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.apartments.com/")
+class Application:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.apartments.com/")
 
-location = input("Enter location: ")
+    # lookup the location user inputted
+    def lookup_searchbox(self, location):
+        element_quickSearch = self.driver.find_element(By.ID, "quickSearchLookup")
+        element_quickSearch.clear()
+        element_quickSearch.send_keys(location)
+        element_quickSearch.send_keys(Keys.RETURN)
 
-element_quickSearch = driver.find_element(By.ID, "quickSearchLookup")
-element_quickSearch.clear()
-element_quickSearch.send_keys(location)
-element_quickSearch.send_keys(Keys.RETURN)
+    def close_app(self):
+        self.driver.close()
 
-input("Waiting...")
-driver.close()
+if __name__ == "__main__":
+    obj = Application()
+    location = input("Please enter a location: ")
+    obj.lookup_searchbox(location)
+    input("Press enter to end program: ")
